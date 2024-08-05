b311dec37e98750856644298a23b486b678b0dbf
Djessy Coiffe <djessy.coiffe@gmail.com>
2024-08-05 17:15:32 +0200

    init project

---
+++ layouts/default.vue
@@ -0,0 +1,10 @@
+<script setup lang="ts"></script>
+
+<template>
+  <div class="bg-slate-900 h-[100vh]">
+    
+    <slot />
+  </div>
+</template>
+
+<style scoped></style>
---
+++ pages/index.vue
@@ -0,0 +1,20 @@
+<template>
+  <div>
+    <h1
+      class='p-10 text-center text-7xl font-semibold bg-gradient-to-r from-blue-600 via-green-500 to-indigo-400 text-transparent bg-clip-text'>
+      FoodLoc
+    </h1>
+      <h2 class="text-white text-center mt-4 font-mono text-xl">the best place to find food near you!</h2>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Home',
+  data() {
+    return {
+
+    }
+  },
+}
+</script>
