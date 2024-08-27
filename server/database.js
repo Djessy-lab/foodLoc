2b3d0aedad38cca606cb4b6c61f7059152760404
Djessy Coiffe <djessy.coiffe@gmail.com>
2024-08-27 12:23:46 +0200

    tmp db

--- server/database.js
+++ server/database.js
@@ -1,6 +1,11 @@
 import Database from 'better-sqlite3';
+import { join } from 'path';
 
-const db = new Database('emails.db', { verbose: console.log });
+const dbPath = process.env.NODE_ENV === 'production'
+  ? '/tmp/emails.db'
+  : join(process.cwd(), 'emails.db');
+
+const db = new Database(dbPath, { verbose: console.log });
 
 db.exec(`
   CREATE TABLE IF NOT EXISTS emails (
